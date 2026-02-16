63de00e331857c3f6ed39e8a0e34cc1bf1f2d478
thinkij <thinkij@levelthree.kr>
2026-02-17 05:21:16 +0900

    aientrophy 적용

--- src/components/common/AientrophyScript.tsx
+++ src/components/common/AientrophyScript.tsx
@@ -4,16 +4,22 @@ import Script from 'next/script';
 
 export default function AientrophyScript() {
     return (
-        <Script
-            src="https://dqf159255eu1e.cloudfront.net/sdk/latest/aientrophy.min.js"
-            strategy="lazyOnload"
-            onLoad={() => {
-                if (typeof window !== 'undefined' && window.Aientrophy) {
-                    window.aientrophy = new window.Aientrophy({
-                        clientKey: 'ae3a5e1c-c07d-419c-b275-086d466158a4',
-                    });
-                }
-            }}
-        />
+        <>
+            <Script
+                id="aientrophy-config"
+                strategy="beforeInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `window.SecuritySDKConfig = {
+                        endpoint: "https://api.aientrophy.com/api/v1/events",
+                        clientKey: "ae3a5e1c-c07d-419c-b275-086d466158a4",
+                        debug: false
+                    };`
+                }}
+            />
+            <Script
+                src="https://dqf159255eu1e.cloudfront.net/sdk/latest/aientrophy.min.js"
+                strategy="afterInteractive"
+            />
+        </>
     );
 }
